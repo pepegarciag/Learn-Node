9e28c95674cf8c81a56d7d360d85608f0a45ee06
Pepe Garcia <pgarciag93@gmail.com>
2018-04-20 09:30:43 +0200

    Lessons 1 to 26

---
+++ controllers/AuthController.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+
+exports.login = passport.authenticate('local', {
+    failureRedirect: '/login',
+    failureFlash: 'Failed login!',
+    successRedirect: '/',
+    successFlash: 'You\'re now logged in!',
+});
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out! 👋');
+  res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next();
+        return;
+    }
+
+    req.flash('error', 'Oops! You must be logged in to do that!');
+    res.redirect('/login');
+};
